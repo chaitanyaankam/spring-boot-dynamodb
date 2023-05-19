3834a0a34cf37a341d4dccbb4057669a6ef9a6ea
Chaitanya19912 <chaitanya.ankam@gmail.com>
2023-05-19 19:24:10 +0530

    in-memory-chache-changes

--- src/main/java/com/example/hazelcaset/dynamo/config/CacheConfig.java
+++ src/main/java/com/example/hazelcaset/dynamo/config/CacheConfig.java
@@ -1,51 +1,22 @@
 package com.example.hazelcaset.dynamo.config;
 
-import com.hazelcast.config.*;
-import com.hazelcast.core.Hazelcast;
-import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.spring.cache.HazelcastCacheManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CacheConfig {
+    @Value("${spring.cache.rest-cache.name}")
+    private String restCacheName;
 
     @Autowired
     private CachePropertiesConfig cachePropertiesConfig;
 
     @Bean
-    public Config hazelCastConfig(){
-        Config config = new Config();
-        config.setClusterName("distributed-demo-cluster");
-        config.getNetworkConfig()
-                .setPort(5701).setPortCount(3).setPortAutoIncrement(true)
-                .getJoin()
-                .getMulticastConfig().setEnabled(true);
-
-        cachePropertiesConfig
-                .getCaches()
-                .forEach(cacheConfig -> {
-                    config.addMapConfig(new MapConfig()
-                            .setName(cacheConfig.getName())
-                            .setTimeToLiveSeconds(cacheConfig.getTtl())
-                            .setEvictionConfig(new EvictionConfig()
-                                    .setSize(cacheConfig.getRecordsInMemory())
-                                    .setMaxSizePolicy(MaxSizePolicy.FREE_HEAP_SIZE)
-                                    .setEvictionPolicy(EvictionPolicy.LRU)));
-        });
-        return config;
+    public CacheManager inMemoryCacheManager() {
+        return new ConcurrentMapCacheManager(restCacheName);
     }
-
-    @Bean
-    public HazelcastInstance hazelcastInstance(Config hazelCastConfig) {
-        return Hazelcast.newHazelcastInstance(hazelCastConfig);
-    }
-
-    @Bean
-    CacheManager cacheManager(HazelcastInstance hazelcastInstance) {
-        return new HazelcastCacheManager(hazelcastInstance);
-    }
-
 }
---
+++ src/main/java/com/example/hazelcaset/dynamo/service/CacheRestCallsService.java
@@ -0,0 +1,29 @@
+package com.example.hazelcaset.dynamo.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class CacheRestCallsService {
+    Logger LOGGER = LoggerFactory.getLogger(CacheRestCallsService.class);
+
+    @Cacheable(cacheNames = "default-rest-cache")
+    public List<String> findAllUsingRestServiceCall() {
+        LOGGER.info("fetching from the REST call.......");
+        List<String> resultFromAPICall = List.of("active pharmacy 1", "active pharmacy 2");
+        LOGGER.info("data from the rest call {} ", resultFromAPICall);
+        return resultFromAPICall;
+    }
+
+    @CacheEvict(value = "default-rest-cache", allEntries = true)
+    @Scheduled(fixedRateString = "${spring.cache.rest-cache.ttl}")
+    public void emptyRestCache() {
+        LOGGER.info("emptying REST cache...");
+    }
+}
